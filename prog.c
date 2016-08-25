401ab947e647465fe7d818e283a19043dead7f0b
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-08-25 17:12:55 -0300

    Make fibi() more readable

--- prog.c
+++ prog.c
@@ -9,7 +9,11 @@ int fibr(int n){
 int fibi(unsigned int n){
 	int a = 1, b = 1, c = 1, i;
 	if(!n) return 0;
-	for(i = 3; i <= n; c = a + b, a = b, b = c, i++);
+	for(i = 3; i <= n; i++){
+		c = a + b;
+		a = b;
+		b = c;
+	}
 	return c;
 }
 
