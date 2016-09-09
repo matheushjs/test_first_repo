91a5619e4924b45064c42f5e5b5fa4c86f28e3d3
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-09-09 19:01:48 -0300

    test

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc6 -Wall prog.c -o prog
+
+clean:
+	rm -f *~ prog
---
+++ prog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+static
+void featureA(){
+	int a;
+	for(a = 0; a < 10; a+=2){
+		a -= 1;
+		printf("%d\n", a);
+	}
+}
+
+int fibr(int n){
+	if(n == 0) return 0;
+	if(n < 3) return 1;
+	return fibr(n-1) + fibr(n-2);
+}
+
+int fibi(unsigned int n){
+	int a = 1, b = 1, c = 1, i;
+	if(!n) return 0;
+	for(i = 3; i <= n; i++){
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
+}
+
+void print_me(){
+	printf("Hello!\n");
+}
+
+int main(int argc, char *argv[]){
+	unsigned int i;
+	for(i = 0; i < 100; i++)
+		printf("f(%d) = %d\n", i, fibi(i));;
+	return 0;
+}
